2c68523824eea383216ad97ae1fcc0ef19d6ff79
UnicornDad8 <unicorndad8@gmail.com>
2024-04-15 18:43:56 -0400

    Added other test scenarios for max function and refactor max function

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -2,7 +2,15 @@ import { describe, test, it, expect } from "vitest";
 import { max } from "../src/intro";
 
 describe("max", () => {
-  it("should return the first argument if is greater", () => {
+  it("should return the first argument if it is greater", () => {
     expect(max(2, 1)).toBe(2);
+  });
+
+  it("should return the second argument if it is greater", () => {
+    expect(max(1, 2)).toBe(2);
+  });
+
+  it("should return the first argument if arguments are equal", () => {
+    expect(max(1, 1)).toBe(1);
   });
 });

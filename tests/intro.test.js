0b4fc443e7258ae592000d516b273c2a5babb8b0
UnicornDad8 <unicorndad8@gmail.com>
2024-04-15 18:35:02 -0400

    Rearrange test in one line of code

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -3,14 +3,6 @@ import { max } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if is greater", () => {
-    // Arrange
-    const a = 2;
-    const b = 1;
-
-    // Act
-    const result = max(a, b);
-
-    // Assert
-    expect(result).toBe(2);
+    expect(max(2, 1)).toBe(2);
   });
 });

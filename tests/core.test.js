c9b02d3e0362c42b5a3e0ebdef904972191f3160
UnicornDad8 <unicorndad8@gmail.com>
2024-04-28 11:10:03 -0400

    Added test suite for canDrive function

--- tests/core.test.js
+++ tests/core.test.js
@@ -5,6 +5,7 @@ import {
   validateUserInput,
   isPriceInRange,
   isValidUsername,
+  canDrive,
 } from "../src/core";
 
 describe("getCoupons", () => {
@@ -137,5 +138,35 @@ describe("isValidUsername", () => {
     expect(isValidUsername(null)).toBe(false);
     expect(isValidUsername(undefined)).toBe(false);
     expect(isValidUsername(1)).toBe(false);
+  });
+});
+
+describe("canDrive", () => {
+  it("should return error for invalid country code", () => {
+    expect(canDrive(20, "FR")).toMatch(/invalid/i);
+  });
+
+  it("should return false for underage in the US", () => {
+    expect(canDrive(15, "US")).toBe(false);
+  });
+
+  it("should return true for min age in the US", () => {
+    expect(canDrive(16, "US")).toBe(true);
+  });
+
+  it("should return true for eligible in the US", () => {
+    expect(canDrive(17, "US")).toBe(true);
+  });
+
+  it("should return false for underage in the UK", () => {
+    expect(canDrive(16, "UK")).toBe(false);
+  });
+
+  it("should return true for min age in the UK", () => {
+    expect(canDrive(17, "UK")).toBe(true);
+  });
+
+  it("should return true for eligible in the UK", () => {
+    expect(canDrive(18, "UK")).toBe(true);
   });
 });

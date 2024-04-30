2ba2e9a56518952ff533871d2490e9d02af48169
UnicornDad8 <unicorndad8@gmail.com>
2024-04-30 11:24:05 -0400

    Added mock test suite for getShippingInfo function

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,8 +1,10 @@
 import { vi, it, expect, describe } from "vitest";
 import { getExchangeRate } from "../src/libs/currency";
-import { getPriceInCurrency } from "../src/mocking";
+import { getShippingQuote } from "../src/libs/shipping";
+import { getPriceInCurrency, getShippingInfo } from "../src/mocking";
 
 vi.mock("../src/libs/currency");
+vi.mock("../src/libs/shipping");
 
 describe("getPriceInCurrency", () => {
   it("should return price in target currency", () => {
@@ -11,5 +13,23 @@ describe("getPriceInCurrency", () => {
     const price = getPriceInCurrency(10, "AUD");
 
     expect(price).toBe(15);
+  });
+});
+
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable if quote cannot be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+
+    const result = getShippingInfo("London");
+
+    expect(result).toMatch(/unavailable/i);
+  });
+
+  it("should return shipping info if quote can be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
+
+    const result = getShippingInfo("London");
+
+    expect(result).toMatch(/shipping cost: \$10 \(2 days\)/i);
   });
 });
